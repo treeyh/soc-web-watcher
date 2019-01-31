d37dd7e61f522d1c1e9ce0d8dacc7356a6be51d2
余海 <tree.yu@dadaabc.com>
2019-01-31 19:03:57 +0800

    调整启动脚本

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#取当前目录
+BASE_PATH=`cd "$(dirname "$0")"; pwd`
+
+cd $BASE_PATH
+
+if [ ! -f "BASE_PATH/venv/bin/activate" ]; then
+    mkdir -p $BASE_PATH/venv
+    /opt/soft/python37/bin/python3 -m venv venv/.
+fi
+
+source $BASE_PATH/venv/bin/activate
+
+pip install -r $BASE_PATH/requirements.txt
+
+echo "nohup python $BASE_PATH/src/start.py >/data/logs/soc-web-watcher/account.log 2>&1 &"
+
+nohup python $BASE_PATH/src/start.py >/data/logs/soc-web-watcher/account.log 2>&1 &
